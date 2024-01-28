c7eddc6b75899d6f9f459c6ccd8179459d897731
soniaakoliya <yuktaakoliya@gmail.com>
2024-01-28 11:34:05 +0530

    changre

--- loginbackend/loginbackend/obj/Debug/net8.0/loginbackend.AssemblyInfo.cs
+++ loginbackend/loginbackend/obj/Debug/net8.0/loginbackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("loginbackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e23ea9a3be20eb6d291ff9dbb538b7d8ba5d2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("loginbackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("loginbackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
